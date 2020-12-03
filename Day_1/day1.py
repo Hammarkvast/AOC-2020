28f8c91e1405f99de582c48df550c948f5deb5a3
Hammarkvast <tomham-3@student.ltu.se>
2020-12-03 21:18:20 +0100

    day 1 done!

--- Day_1/day1.py
+++ Day_1/day1.py
@@ -12,12 +12,29 @@ def accounting(expenseReport):
         else:
             index+=1
 
+def moreAccounting(expenseReport):
+    stationary = 0
+    index = 1
+    nextIndex = 2
+    while (stationary <= (len(expenseReport)-2) and (index <= (len(expenseReport)-1)) and (nextIndex <= len(expenseReport))):
+        if (nextIndex == len(expenseReport)) and (index == (len(expenseReport) - 1)):
+            stationary+=1
+            index = stationary + 1
+            nextIndex = index + 1
+        elif (expenseReport[stationary] + expenseReport[index] + expenseReport[nextIndex]) == 2020:
+            return expenseReport[stationary] * expenseReport[index] * expenseReport[nextIndex]
+        else:
+            if(index < (len(expenseReport)-1) and (nextIndex  == (len(expenseReport)-1))):
+                nextIndex = index + 1
+                index+=1
+            else:
+                nextIndex+=1
+
 
 with open('input.txt') as f:
     input = [line.rstrip() for line in f]
 
 for i in range(0, len(input)): 
     input[i] = int(input[i]) 
-# print(len(input))
 
-print(accounting(input))
+print(moreAccounting(input))
