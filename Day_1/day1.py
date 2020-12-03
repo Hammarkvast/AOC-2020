0dfbe640e88e9aeec3883f3f9c4248ea625db93d
Hammarkvast <tomham-3@student.ltu.se>
2020-12-03 12:30:39 +0100

    AOC day_1.1 done

--- Day_1/day1.py
+++ Day_1/day1.py
@@ -1,20 +1,23 @@
-input = [979,675,366,299,1721]
+# input = [979,675,299,366,1721]
+input = []
 def accounting(expenseReport):
     stationary = 0
     index = 1
     while (stationary <= (len(expenseReport)-1)) and (index <= len(expenseReport)):
-        print("test")
         if index == len(expenseReport):
-            print("testing")
             stationary+=1
             index = stationary + 1
         elif (expenseReport[stationary] + expenseReport[index]) == 2020:
-            print("HELLO")
             return expenseReport[stationary]*expenseReport[index]
         else:
-            print(index)
             index+=1
 
 
+with open('input.txt') as f:
+    input = [line.rstrip() for line in f]
+
+for i in range(0, len(input)): 
+    input[i] = int(input[i]) 
+# print(len(input))
 
 print(accounting(input))
